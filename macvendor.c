5f8ecd620c4861b765cebe9c981fce921dc76af5
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2010-11-30 22:52:11 +0100

    Initial englishhhh commit

--- macvendor.c
+++ macvendor.c
@@ -1,82 +1,73 @@
+#include <ctype.h>
+#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-#define VALMISTAJAT "/home/weezel/ohjelmointi/c/macvendor/vendors.txt"
+#ifndef VENDORS_FILE
+#define VENDORS_FILE "/home/weezel/apps/macvendor/vendors.txt"
+#endif
 
-int isInLine( char *p, char *r );
+int
+isInLine(char *, char *);
 
-int main( int argc, char **argv )
+int
+main(int argc, char *argv[])
 {
     FILE *input = NULL;
-    /*char *vertailu;*/
-    char etsittava[8];
-    char valmistaja[256];
-    int loytyi = 0;
-    int i = 0;
+    char searchitem[9], vendor[256]; /* TODO Use malloc */
+    int found, i, j;
+    found = i = j = 0;
 
-    if( argc != 2 )
-    {
-        /* Ei saatu MAC-osoitetta parametrina */
-        fprintf( stderr, "Give a mac address as a parameter.\n" );
-        fprintf( stderr, "For example: macvendor 00-00-00\n");
-        exit( 1 );
-    }
-    
-    if( (input = fopen( VALMISTAJAT, "r" )) == NULL )
-    {
-        /* Valmistajien tietuetta ei saatu auki */
-        fprintf( stderr, "Valmistajien tietuetta ei saatu auki.\n" );
-        fprintf( stderr, "Varmista, että vendors.txt on samassa hakemistossa ohjelman kanssa.\n" );
-        exit( 1 );
-    }
+    if (argc < 2) {
+	extern char *__progname;
+	fprintf(stderr, "usage: %s MAC-address DB-name\n", __progname);
+	exit(1);
+    } 
 
-    if( strlen( argv[1] ) < 8 )
-    {
-        fprintf( stderr, "Too short for a mac address.\n" );
-        exit( 1 );
-    }
-    
-    /* Siistitään mahdollisesti parametrina saatua väärää syntaksia */
-    for( i = 0; i < 8; i++)
-    {
-        if( argv[1][i] == ':' )
-            etsittava[i] = '-';
+    if (strlen(argv[1]) < 8)
+        errx(3, "%s\n", "Too short for a mac address.");
+
+#ifndef VENDORS_FILE
+    if ((input = fopen(argv[2], "r")) == NULL)
+	errx(2, "Could not open the database: %s\n", argv[2]);
+#else
+    if ((input = fopen(VENDORS_FILE, "r")) == NULL)
+	errx(2, "Could not open the database: %s\n", argv[2]);
+#endif
+
+    for (i = 0, j = 1; i < 8; i++, j++) {
+	if (j % 3 == 0)
+            searchitem[i] = '-';
         else
-            etsittava[i] = toupper( argv[1][i] );
+            searchitem[i] = toupper(argv[1][i]);
     }
-    
-    /* Etsitään vastaavaa tietuetta tekstitiedostosta */
-    while( fgets( valmistaja, sizeof( valmistaja ), input ) != NULL )
-    {
-        if( isInLine( etsittava, valmistaja ) == 1)
-        {
-            printf( "%s", valmistaja );
-            loytyi = 1;
+
+    searchitem[9] = '\0';
+
+    while (fgets(vendor, sizeof(vendor), input) != NULL) {
+        if (isInLine(&searchitem[0], &vendor[0]) == 1) {
+            fprintf(stdout, "%s", vendor);
+            found = 1;
         }
     }
-    fclose( input );
-    
-    if( loytyi == 0 )
-    {
-        fprintf( stderr, "Kyseistä valmistajaa ei löytynyt.\n" );
-        exit( 1 );
-    }
+
+    fclose(input);
+
+    if (found == 0)
+        errx(3, "MAC %s not found.", searchitem);
 
     return 0;
 }
 
 
-int isInLine( char *p, char *r )
+int
+isInLine(char *p, char *r)
 {
-    /* Mikäli 8 ensimmäistä kirjainta vastaavat toisiaan,
-     * palautetaan 1 eli true, muussa tapauksessa 0 eli false.
-     */
     int i = 0;
-    for( i = 0; i < 8; i++)
-    {
-        if( p[i] != r[i] )
+
+    for (i = 0; i < 8; i++) {
+        if (*(&p[i]) != *(&r[i]))
             return 0;
     }
     return 1;
