94e825b5bd93d22a5bdb901fa37baa0a093d92b0
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2011-04-28 00:05:24 +0200

    Ignore statement from the previous commit - POSIX way is ok, just drop -ansi switch while compiling. netfetch.c speaks http 1.1 now.

--- Makefile
+++ Makefile
@@ -5,10 +5,11 @@ all: macvendor.c
 	mv -f macvendor ${HOME}/bin
 
 netfetch: netfetch.c
-	${CC} -g -ansi -Wall -o netfetch netfetch.c
+	${CC} -Wall -o netfetch netfetch.c
 
 debug:
 	${CC} -g -ansi -Wall -o macvendor macvendor.c
+	${CC} -g -Wall -o netfetch netfetch.c
 
 clean:
 	rm -f macvendor *.core
--- netfetch.c
+++ netfetch.c
@@ -2,28 +2,34 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
+#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
-#define HTTP_ADDRESS "http://standards.ieee.org/regauth/oui/oui.txt"
+#define HTTP_HOST "standards.ieee.org"
+#define HTTP_PATH "/develop/regauth/oui/oui.txt"
 #define HTTP_PORT "80"
 #define USER_AGENT "Mozilla"
 
+char *build_get_query();
+
 int main()
 {
-	int				bytes, sock, gaierr;
 	struct addrinfo hints, *res;
-	char			buffer[BUFSIZ + 1];
-	char			*hostname = "time.nist.gov";
+	int		 bytes, gaierr, sent, sock, tmpres;
+	char	 buffer[BUFSIZ + 1];
+	char	*get;
 
+	bytes = sent = tmpres = 0;
+
+	memset(&buffer, 0, sizeof(buffer));
 	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 
-	if ((gaierr = getaddrinfo(hostname,
-					HTTP_PORT, &hints, &res)) != 0) {
+	if ((gaierr = getaddrinfo(HTTP_HOST, HTTP_PORT, &hints, &res)) != 0) {
 		fprintf(stderr, "getaddrres error %d: %s\n",
 				gaierr, gai_strerror(gaierr));
 		return (2);
@@ -40,13 +46,40 @@ int main()
 		return (2);
 	}
 
+	get = build_get_query();
+
+	/* Send the query to the server */
+	while (sent < strlen(get)) { 
+		if ((tmpres = send(sock, get + sent, strlen(get) - sent, 0)) == -1)
+			err(1, "Cannot send query");
+		sent += tmpres;
+	}
+
 	while ((bytes = read(sock, buffer, BUFSIZ)) > 0)
 		fprintf(stdout, "%s\n", buffer);
 		/*write(stdout, buffer, bytes);*/
 
 	close(sock);
 	freeaddrinfo(res);
+	free(get);
 
 	return (0);
+}
+
+char
+*build_get_query()
+{
+	int		 qsize = 0;
+	char	*query = NULL;
+	const char *header = "GET /%s HTTP/1.1\nHost: %s\nUser-Agent: %s\n\nConnection: close\n\n";
+
+	/* -5 is to consider the %s %s %s in tpl and the ending \0 */
+	qsize = strlen(HTTP_HOST) + strlen(HTTP_PATH) + strlen(USER_AGENT) + strlen(header) - 5;
+
+	if ((query = calloc(qsize, sizeof(char *))) == NULL)
+		  err(1, "calloc failure in build_get_query");
+	sprintf(query, header, HTTP_PATH, HTTP_HOST, USER_AGENT);
+
+	return query;
 }
 
