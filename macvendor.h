4620f47cb02b4ddb81c24d925f7147f182e7a220
Ville Valkonen <weezelding@gmail.com>
2015-08-17 21:55:38 +0300

    Location of vendors.txt has changed.

--- macvendor.h
+++ macvendor.h
@@ -6,7 +6,7 @@
 #define VENDORS_FILE "/home/weezel/mnt/store/ohjelmointi/c/macvendor/vendors.txt"
 #endif
 #if defined __linux__
-#define VENDORS_FILE "/home/weezel/ohjelmointi/c/macvendor/vendors.txt"
+#define VENDORS_FILE "/home/weezel/mnt/store/ohjelmointi/c/macvendor/vendors.txt"
 #endif
 #else
 #define VENDORS_FILE "vendors.txt"
